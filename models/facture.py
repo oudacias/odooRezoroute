14e2fb44f35449c2a6812419af36832c1ba78d4d
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-24 12:55:50 +0100

    from mac

--- models/facture.py
+++ models/facture.py
@@ -4,9 +4,13 @@ class AcoountMoveExtra(models.Model):
 
     _inherit = 'account.move'
 
+    session_id = fields.Many2one('pos.session',string="Session id")
+
 
 
     def create(self,vals):
+
+        print("Creating new account")
 
         session = self.env['pos.session'].search([('user_id','=',self.env.uid),('state','=','opening_control')])  
 
