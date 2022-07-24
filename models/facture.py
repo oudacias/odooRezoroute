9b85de4a722ffa32c14a6b0601bef74f855f2623
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-24 13:52:17 +0100

    from mac

--- models/facture.py
+++ models/facture.py
@@ -3,9 +3,7 @@ from odoo import fields, models,api
 class AcoountMoveExtra(models.Model):
 
     _inherit = 'account.move'
-
     session_id = fields.Many2one('pos.session',string="Session id")
-
 
     @api.model
     def create(self,vals):
@@ -21,4 +19,18 @@ class AcoountMoveExtra(models.Model):
 
         q= super(AcoountMoveExtra, self).create(vals) 
         return q
+
+
+class PosData(models.Model):
+
+    _inherit = 'pos.session'
+
+    facture_count = fields.Integer(compute='_compute_facture_count')
+
+
+    def _compute_facture_count(self):
+        orders_data = self.env['account.move'].read_group([('session_id', 'in', self.ids)], ['session_id'], ['session_id'])
+        sessions_data = {order_data['session_id'][0]: order_data['session_id_count'] for order_data in orders_data}
+        for session in self:
+            session.facture_count = sessions_data.get(session.id, 0)
     
