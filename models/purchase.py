abe025fdfd28ee91f25d73c25befa886c3807ad2
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-27 10:55:35 +0100

    from mac

--- models/purchase.py
+++ models/purchase.py
@@ -5,10 +5,24 @@ class purchase_custom(models.Model):
     _inherit = 'purchase.order'
 
 
-    # def button_confirm(self):
-    #     res = super(purchase_custom, self).button_confirm()
-    #     res.write('')
-    #     return res 
+    def button_confirm(self):
+        # res = super(purchase_custom, self).button_confirm()
+        purchase_line = self.env['purchase.order.line'].search('order_id','=',self.id)
+        print('Purchase line_ids   '+ str(purchase_line.id))
+        for line in purchase_line:
+            stock_move = self.env['stock.move'].search('purchase_line_id','=',line.id)
+            print('Stock Move   '+ str(stock_move.id))
+
+            stock_move_line = self.env['stock.move.line'].search('move_id','=',stock_move.id)
+            for line in stock_move_line:
+                print('Stock Move  Line  '+ str(line.id))
+
+                picking_id = self.env['stock.move.line'].search('move_id','=',stock_move.id)
+
+                print('Picking line_ids   '+ str(picking_id.id))
+
+
+        # return res 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
