04661aac3efb20638c24ef30b5df2b73e3bdcc47
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-27 12:00:23 +0100

    from mac

--- models/purchase.py
+++ models/purchase.py
@@ -9,29 +9,33 @@ class purchase_custom(models.Model):
         res = super(purchase_custom, self).button_confirm()
         print("@@@@self Order: " + str(self.id))
         purchase_line = self.env['purchase.order.line'].search([('order_id','=',self.id)])
+
+        picking_id = self.env['stock.picking'].search([('purchase_id','=',self.id)])
+        picking_id.write({'location_dest_id':9})
         
-        for line in purchase_line:
-            print('Purchase line_ids   '+ str(line.id))
-            stock_move = self.env['stock.move'].search([('purchase_line_id','=',line.id)])
-            print('Stock Move   '+ str(stock_move.location_dest_id))
-            stock_move.write({'location_dest_id':9})
-            stock_move = self.env['stock.move'].search([('purchase_line_id','=',line.id)])
-            print('Stock Move new   '+ str(stock_move.location_dest_id))
+        # for line in purchase_line:
+        #     print('Purchase line_ids   '+ str(line.id))
+        #     stock_move = self.env['stock.move'].search([('purchase_line_id','=',line.id)])
+        #     print('Stock Move   '+ str(stock_move.location_dest_id))
+        #     stock_move.write({'location_dest_id':9})
+        #     stock_move = self.env['stock.move'].search([('purchase_line_id','=',line.id)])
+        #     print('Stock Move new   '+ str(stock_move.location_dest_id))
 
 
-            stock_move_line = self.env['stock.move.line'].search([('move_id','=',stock_move.id)])
-            for line in stock_move_line:
-                print('Stock Move  Line  '+ str(line.id))
-                line.write({'location_dest_id':9})
+            # stock_move_line = self.env['stock.move.line'].search([('move_id','=',stock_move.id)])
+            # for line in stock_move_line:
+            #     print('Stock Move  Line  '+ str(line.id))
 
-                picking_id = self.env['stock.move.line'].search([('move_id','=',stock_move.id)])
+            #     line.write({'location_dest_id':9})
+
+            #     picking_id = self.env['stock.picking'].search([('move_id','=',stock_move.id)])
                 
-                picking_id.write({'location_dest_id':9})
+            #     picking_id.write({'location_dest_id':9})
 
-                print('Picking line_ids   '+ str(picking_id.id))
+            #     print('Picking line_ids   '+ str(picking_id.id))
 
 
-        # return res 
+        return res 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
