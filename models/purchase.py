93e0a5f0a8be36285fd83e63f16d9aa4c8dc0252
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-27 11:17:29 +0100

    from mac

--- models/purchase.py
+++ models/purchase.py
@@ -13,8 +13,11 @@ class purchase_custom(models.Model):
         for line in purchase_line:
             print('Purchase line_ids   '+ str(line.id))
             stock_move = self.env['stock.move'].search([('purchase_line_id','=',line.id)])
-            print('Stock Move   '+ str(stock_move.id))
+            print('Stock Move   '+ str(stock_move.location_dest_id))
             stock_move.write({'location_dest_id':9})
+            stock_move = self.env['stock.move'].search([('purchase_line_id','=',line.id)])
+            print('Stock Move new   '+ str(stock_move.location_dest_id))
+
 
             stock_move_line = self.env['stock.move.line'].search([('move_id','=',stock_move.id)])
             for line in stock_move_line:
