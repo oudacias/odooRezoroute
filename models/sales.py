539a6b88e17e508ad80f77610718969d90461561
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-30 14:52:51 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -83,24 +83,46 @@ class SaleOrderExtra(models.Model):
     def action_confirm(self):
 
         print("CONFIRMATION ACTION  @@@@@@@@@@@@@")
-        # if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-        #     raise UserError(_(
-        #         'It is not allowed to confirm an order in the following states: %s'
-        #     ) % (', '.join(self._get_forbidden_state_confirm())))
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
 
-        # for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-        #     order.message_subscribe([order.partner_id.id])
-        # self.write(self._prepare_confirmation_values())
+        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+            order.message_subscribe([order.partner_id.id])
+        self.write(self._prepare_confirmation_values())
 
-        # # Context key 'default_name' is sometimes propagated up to here.
-        # # We don't need it and it creates issues in the creation of linked records.
-        # context = self._context.copy()
-        # context.pop('default_name', None)
+        # Context key 'default_name' is sometimes propagated up to here.
+        # We don't need it and it creates issues in the creation of linked records.
+        context = self._context.copy()
+        context.pop('default_name', None)
 
-        # self.with_context(context)._action_confirm()
-        # if self.env.user.has_group('sale.group_auto_done_setting'):
-        #     self.action_done()
-        # return True
+        self.with_context(context)._action_confirm()
+        if self.env.user.has_group('sale.group_auto_done_setting'):
+            self.action_done()
+
+
+
+        # Change stock location
+        picking_id = self.env['stock.picking'].search([('purchase_id','=',self.id)])
+        picking_id.write({'location_id':9})
+
+        stock_move = self.env['stock.move'].search([('picking_id','=',picking_id.id)])
+        stock_move.write({'location_id':9})
+
+        
+        for line in stock_move.move_line_ids:
+            line.write({'location_id':9})
+
+        # Change stock location -- END
+
+
+
+
+
+
+
+        return True
 
 
 
