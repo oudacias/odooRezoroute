f89aa87aabe3efa1afff81c489e81a76a023272d
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-07-30 14:46:27 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -1,6 +1,8 @@
 from sre_parse import State
 from datetime import date
 from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
+
 
 
 from odoo import fields, models,api
@@ -78,6 +80,29 @@ class SaleOrderExtra(models.Model):
             self.hide_confirm = True
 
 
+    def action_confirm(self):
+
+        print("CONFIRMATION ACTION  @@@@@@@@@@@@@")
+        # if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+        #     raise UserError(_(
+        #         'It is not allowed to confirm an order in the following states: %s'
+        #     ) % (', '.join(self._get_forbidden_state_confirm())))
+
+        # for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+        #     order.message_subscribe([order.partner_id.id])
+        # self.write(self._prepare_confirmation_values())
+
+        # # Context key 'default_name' is sometimes propagated up to here.
+        # # We don't need it and it creates issues in the creation of linked records.
+        # context = self._context.copy()
+        # context.pop('default_name', None)
+
+        # self.with_context(context)._action_confirm()
+        # if self.env.user.has_group('sale.group_auto_done_setting'):
+        #     self.action_done()
+        # return True
+
+
 
 
     
@@ -89,13 +114,14 @@ class SaleOrderExtra(models.Model):
     def sale_order_to_repair_order(self):
         self.hide_confirm = True
         self.write({'state':'repair_order','hide_confirm' : True})
-        self.env['sale.order'].action_confirm()
+        # self.env['sale.order'].action_confirm()
 
     def sale_order_making(self):
         self.write({'state':'making'})
 
     def action_button_confirm_repair_order(self):
         self.write({'state':'progress'})
+        self.env['sale.order'].action_confirm()
 
 
     def action_order_deposit(self):
