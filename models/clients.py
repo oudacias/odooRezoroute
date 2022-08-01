70ccc88e6c0e74af4e13463c153efc5cc8313a4e
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-01 08:23:37 +0100

    from mac

--- models/clients.py
+++ models/clients.py
@@ -13,6 +13,9 @@ class PartnerExtra(models.Model):
     partner_invoice_id = fields.Many2one('res.partner','Adresse de facturation')
     partner_adress_on_invoice_id = fields.Many2one('res.partner','Adresse a imprimer sur les factures')
 
+    is_cheque_flotte = fields.Boolean(string = 'Chèque flotte ?')
+    pourcentage_remise = fields.Float(string = 'Pourcentage de la Remise ')
+
  
 
 
