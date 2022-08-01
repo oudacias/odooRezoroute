69549736b78910a0136895f58438dcd370c0fe1a
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-01 12:41:21 +0100

    from mac

--- models/validatepos.py
+++ models/validatepos.py
@@ -1,7 +1,9 @@
 from odoo import models, fields
 
 class ValidateSession(models.Model):
+
    _inherit = 'pos.session'
+   
    def action_done(self):
        for rec in self:
             if (rec.state == "opening_control"):
