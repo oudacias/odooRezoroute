c854cb6f6abfc0fff2245eeba016e00d5c36e5f2
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-01 09:32:52 +0100

    from mac

--- models/pos.py
+++ models/pos.py
@@ -35,7 +35,7 @@ class PosSession(models.Model):
         return {
             'view_mode': 'form',
             'res_model': 'pos.session',
-            'view_id': 'pos_fond_wizard_form',
+            'view_id': self.env.ref('ps_rezoroute.pos_fond_wizard_form').id,,
             'target' : 'new',
             'type': 'ir.actions.act_window',
             'context' : {'default_id' : self.id }
