925759c368273f9d7b8f0c8f585c6ac8bc5c7984
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-02 13:29:31 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -293,14 +293,14 @@ class ConfirmRepairOrder(models.Model):
     def confirm_order(self):
 
         rec =  self.env['sale.order'].search([],order="id desc", limit =1)  
-        for h in rec:
-            h.write({'state':'sale'})
+        # for h in rec:
+        #     h.write({'state':'sale'})
 
         # rec = self.env['sale.order'].search([('id','=',self.id)], limit=1)
 
         # sale_id = self.env['sale'].search([('id','=',self.id)], limit=1)
 
         # sale_id.write({'state':'progress'})
-        # super(SaleOrderExtra, self).action_confirm()
+        super(SaleOrderExtra, self).action_confirm()
 
 
