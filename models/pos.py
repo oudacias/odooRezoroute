549e5eb6abdbcae499879283715571c094d95184
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-05 00:32:48 +0100

    from mac

--- models/pos.py
+++ models/pos.py
@@ -109,7 +109,7 @@ class PosSession(models.Model):
 
 
         data=[]
-        for a in self.env('account.payment').search([]) : 
+        for a in self.env['account.payment'].search([]) : 
             total = 0
             for ligne in  self.payment_id:
                 if ligne.id==a.id:
