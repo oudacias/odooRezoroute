846ecc29245076c1b3daccb5c061a9bbbf6be980
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-04 11:20:57 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -278,13 +278,13 @@ class SaleOrderExtra(models.Model):
                 }  
             else:
 
-                stock_picking = self.env['stock.picking'].search([('sale_id','=',self.id)])
+                # stock_picking = self.env['stock.picking'].search([('sale_id','=',self.id)])
 
 
 
-                print("Product prices   ids " +str(stock_picking))
-                stock_picking.move_lines._set_quantities_to_reservation()
-                stock_picking.button_validate()
+                # print("Product prices   ids " +str(stock_picking))
+                # stock_picking.move_lines._set_quantities_to_reservation()
+                # stock_picking.button_validate()
                 
                 active_ids = self._context.get('active_ids')
                 a = self.env['account.move'].browse(active_ids)
