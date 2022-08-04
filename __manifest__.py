c93404e567e240193b3064d04a526cab41acb0a7
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-04 16:25:14 +0100

    from mac

--- __manifest__.py
+++ __manifest__.py
@@ -24,8 +24,9 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/products.xml',
