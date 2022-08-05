1e1741dfcdae9fe0c3236805e1eb203f70d45130
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-05 10:35:31 +0100

    from mac

--- models/purchase.py
+++ models/purchase.py
@@ -5,31 +5,34 @@ from odoo import fields, models,api
 class purchase_custom(models.Model):
     _inherit = 'purchase.order'
     session_id = fields.Many2one('pos.session',string="Session id")
+    location_name = fields.Char(compute="_get_location_name",string="Emplacement")
     location_id = fields.Char(compute="_get_location_name",string="Emplacement")
+    
 
 
     def _get_location_name(self):
         location_dest_id = self.env['pos.config'].search([('user_id','=',self.env.uid)], limit=1)
-        self.location_id = location_dest_id.location_id.complete_name
+        self.location_name = location_dest_id.location_id.complete_name
+        self.location_id = location_dest_id.location_id.id
 
 
 
     def button_confirm(self):
         res = super(purchase_custom, self).button_confirm()
 
-        location_dest_id = self.env['pos.config'].search([('user_id','=',self.env.uid)], limit=1)
+        # location_dest_id = self.env['pos.config'].search([('user_id','=',self.env.uid)], limit=1)
 
 
 
         picking_id = self.env['stock.picking'].search([('purchase_id','=',self.id)])
-        picking_id.write({'location_dest_id':location_dest_id.location_id.id})
+        picking_id.write({'location_dest_id':self.location_id.id})
 
         stock_move = self.env['stock.move'].search([('picking_id','=',picking_id.id)])
-        stock_move.write({'location_dest_id':location_dest_id.location_id.id})
+        stock_move.write({'location_dest_id':self.location_id.id})
 
         
         for line in stock_move.move_line_ids:
-            line.write({'location_dest_id':location_dest_id.location_id.id})
+            line.write({'location_dest_id':self.location_id.id})
 
         return res 
 
