4441aef4661d390800be29eab883efb174b265c5
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-06 18:56:00 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -173,6 +173,10 @@ class SaleOrderExtra(models.Model):
 
         # Change stock location -- END
 
+        
+        for rec in picking_id.move_ids_without_package:
+            print("@@@@ Quantity Product Pickings   formed from: " + str(rec.product_uom_qty))
+
         print("CONFIRMATION ACTION  @@@@@@@@@@@@@ END")
 
         return True
