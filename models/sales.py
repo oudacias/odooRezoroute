81bbd0bc9382a4119f12f88d635d033bbc9813a9
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-18 14:09:30 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -52,6 +52,7 @@ class SaleOrderExtra(models.Model):
 
     def _paid_check(self):
         self.paid_check = False
+        print("@@@ Payment Satus @@@@@ 1  "  +str(len(self.invoice_ids)))
         if(len(self.invoice_ids) > 0 ):
             print("@@@ Payment Satus @@@@@   "  +str(self.invoice_ids.payment_state))
             if(self.invoice_check == True):
