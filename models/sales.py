73fb3162d274a9f464e5697bcb6b73bf55161c50
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-18 13:59:53 +0100

    from mac

--- models/sales.py
+++ models/sales.py
@@ -41,6 +41,9 @@ class SaleOrderExtra(models.Model):
         if(len(self.invoice_ids) > 0 and self.state == "sale"):
             if(self.invoice_ids.payment_state == 'paid'):
                 self.invoice_check = True
+            else:
+                self.invoice_check = False
+
         elif(self.state == "sale"):
             self.invoice_check = True
         else:
