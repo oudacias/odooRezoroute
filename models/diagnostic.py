8318e28d7499be486dfc3ced0839e18e27af13c4
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-22 23:20:45 +0100

    from mac

--- models/diagnostic.py
+++ models/diagnostic.py
@@ -8,6 +8,7 @@ class EnginDiagnostic(models.Model):
     is_default = fields.Boolean(string="Par défaut")
     active = fields.Boolean(string="Active")
     engin_diagnostic_line = fields.One2many('engin.diagnostic.line','diagnostic_id')
+    diagnostic_sale_id = fields.Many2one('sale.order')
 
 
 class EnginDiagnosticLine(models.Model):
@@ -17,3 +18,24 @@ class EnginDiagnosticLine(models.Model):
     sequence = fields.Integer(string="Séquence")
     description = fields.Text(string="Description de la tâche")
     diagnostic_id = fields.Many2one('engin.diagnostic')
+
+class DevisDiagnostic(models.Model):
+
+    _inherit = 'sale.order'
+    engin_diagnostic_sale = fields.One2many('engin.diagnostic','diagnostic_sale_id')
+
+    def add_diagnostic(self):
+        return {
+            'res_model': 'engin.diagnostic.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'type': 'ir.actions.act_window',
+            'views' : [(False, 'form')],
+            
+            }  
+class DiagnosticWizard(models.Model):
+
+    name = 'engin.diagnostic.wizard'
+    diagnostic_id = fields.Many2one('engin.diagnostic')
+    sale_id = fields.Many2one('sale.order')
+
