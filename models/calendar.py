0165c48509cb8eb800ad130b2d6701d11bc0e349
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-22 12:01:04 +0100

    from mac

--- models/calendar.py
+++ models/calendar.py
@@ -5,4 +5,10 @@ class CalendarExtra(models.Model):
     _inherit = 'calendar.event'
     sale_id = fields.Many2one('sale.order',string="Commande de vente")
     engin_id = fields.Many2one('fleet.vehicle',string="Véhicule")
+    calendar_count = fields.Integer(compute="_compute_calendar")
+
+    def _compute_calendar(self):
+        Calendar = self.env['calendar.event']
+        for record in self:
+            record.calendar_count = Calendar.search_count([('engin_id', '=', record.id)])
 
