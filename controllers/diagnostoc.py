536ac9ce25a6f3195db8edf9624d5e4f955a3b19
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-23 18:37:48 +0100

    from mac

--- controllers/diagnostoc.py
+++ controllers/diagnostoc.py
@@ -18,11 +18,11 @@ class Diagnostic(http.Controller):
         line_list = request.httprequest.form.getlist('line_id')
         comment_list = request.httprequest.form.getlist('comment')
         next_reminder_list = request.httprequest.form.getlist('next_reminder')
-        done_list = request.httprequest.form
+        done_list = request.httprequest.form.items()
         print("@@@@ Hello ")
         print(type(done_list))
         print(str(done_list))
-        print(str(done_list[0]))
+        # print(str(done_list[0]))
 
         # for i in range(len(line_list)):
             # line_diagnostic = request.env['sale.order'].search([('id','=',line_list[i])])
