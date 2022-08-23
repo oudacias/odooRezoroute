6e495a342e6a94591c41b45c872d8f51852eb115
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-23 15:59:35 +0100

    from mac

--- controllers/diagnostoc.py
+++ controllers/diagnostoc.py
@@ -15,7 +15,7 @@ class Diagnostic(http.Controller):
     @http.route('/update/diagnostic',type="http", website=True ,auth='public')
     def update_diagnostic(self, **kw):
        
-        print("@@@@ Hello " +str(kw.get('line_id')[1]))
+        print("@@@@ Hello " +str(kw.get('line_id')))
         return http.request.render('ps_rezoroute.update_diagnostic_done', {})
 
     # @http.route('/ps_rezoroute/ps_rezoroute/objects', auth='public')
