a6a2467975a123051e676c481fde08037c10136d
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-23 11:14:49 +0100

    from mac

--- models/diagnostic.py
+++ models/diagnostic.py
@@ -19,6 +19,9 @@ class EnginDiagnosticLine(models.Model):
     diagnostic_id = fields.Many2one('engin.diagnostic')
     diagnostic_sale_id = fields.Many2one('sale.order')
 
+    comment = fields.Char(string="Observations/Commentaires")
+    next_reminder = fields.Selection([('jamais','Jamais'),('immediat','Immédiat'),('1 mois','1 mois'),('2 mois','2 mois'),('3 mois','3 mois'),('6 mois','6 mois'),('9 mois','9 mois'),('1 ans','1 ans'),('2 ans','2 ans'),('3 ans','3 ans')])
+    done = fields.Boolean(string="Tâche effectuée ou non")
 
 
 class DevisDiagnostic(models.Model):
@@ -50,9 +53,9 @@ class DiagnosticWizard(models.Model):
 
         for rec in diagnostic_id.engin_diagnostic_line:
 
-            if(rec.id != sale_id.engin_diagnostic_sale.diagnostic_id.id):
+            # if(rec.id != sale_id.engin_diagnostic_sale.diagnostic_id.id):
 
-                sale_id.update({'engin_diagnostic_sale': [(0, 0, {'name':rec.name, 'sequence': rec.sequence, 'description': rec.description})]})
+            sale_id.update({'engin_diagnostic_sale': [(0, 0, {'name':rec.name, 'sequence': rec.sequence, 'description': rec.description})]})
                 
 
 
