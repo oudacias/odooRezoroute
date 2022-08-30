2287a4b5e3abf0dd5db7ad66189ad6292385f391
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-30 21:50:05 +0100

    from mac

--- models/devis.py
+++ models/devis.py
@@ -74,7 +74,7 @@ class Devis(models.Model):
     @api.onchange('order_line')
     def check_line(self):
         print("@@@@@@@ ######## Check Order Line #####" + str(self.order_line.product_id))
-        ctx_lines = self.order_line.mapped('id')
+        ctx_lines = self.order_line._origin.mapped('id')
         # for ctx_line in ctx_lines:
         print("@@@ ######## Check Order Line #####")
         print(str(ctx_lines))
