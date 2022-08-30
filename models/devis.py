6ec5dfd7ffc44b1aa3b4720fc810f6752df5f339
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-30 14:17:46 +0100

    from mac

--- models/devis.py
+++ models/devis.py
@@ -73,8 +73,9 @@ class Devis(models.Model):
 
     @api.onchange('order_line')
     def check_line(self):
-        print("@@@@@@@ ######## Check Order Line #####" + str(self))
-        raise ValidationError('Le numéro du téléphone doit contenir 10 chiffres')
+        print("@@@@@@@ ######## Check Order Line #####" + str(self.sale_order.product_id))
+        # if(self.sale_order.)
+        raise ValidationError('Vous ne pouvez pas supprimer cette ligne du forfait')
 
 
     
