5481a7eeba2d850c17108a236f5276989c3a6117
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-31 17:31:39 +0100

    from mac

--- models/devis.py
+++ models/devis.py
@@ -86,18 +86,18 @@ class Devis(models.Model):
         # if self.order_line:
         #     if()
         
-        print("@@@@@@@ ######## Check Order Line #####" + str(self.order_line._ids))
+        print("@@@@@@@ ######## Check Order Line #####" + str(self.order_lines.new()))
         ctx_lines = self._origin.order_line.mapped('id')
         ctx_lines1 = self.order_line.mapped('id')
         # if(len(ctx_lines) < len(ctx_lines1)):
-        for ctx_line in  self.order_line:
-            if(ctx_line.id):
-                print("@@@@@@ I AM VERY TIRED  " +str(ctx_line.id))
-            # if ctx_line[0] in (0,1) and ctx_line[2].get('xvalue', False):
-            print(str(ctx_line.NewId()))
-        for ctx_line in  self._origin.order_line:
-            # if ctx_line[0] in (0,1) and ctx_line[2].get('xvalue', False):
-            print("111 @@@ ######## Check Order Line #####" + str(ctx_line[0]) + " #####")
+        # for ctx_line in  self.order_line:
+        #     if(ctx_line.id):
+        #         print("@@@@@@ I AM VERY TIRED  " +str(ctx_line.id))
+        #     # if ctx_line[0] in (0,1) and ctx_line[2].get('xvalue', False):
+        #     print(str(ctx_line.NewId()))
+        # for ctx_line in  self._origin.order_line:
+        #     # if ctx_line[0] in (0,1) and ctx_line[2].get('xvalue', False):
+        #     print("111 @@@ ######## Check Order Line #####" + str(ctx_line[0]) + " #####")
         # print(str(xstate))
         # print(str(ctx_lines1))
     
