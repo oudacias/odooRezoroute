f0594635e4884506f3964b629a04765e2dc31155
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-08-31 13:53:57 +0100

    from mac

--- models/devis.py
+++ models/devis.py
@@ -80,7 +80,7 @@ class Devis(models.Model):
         xlast = False
         #for identify non saved lines
         index = 0
-        print("@@@@@@@ ######## Check Order Line #####" + str(self.order_line.target()))
+        print("@@@@@@@ ######## Check Order Line #####" + str(self.target()))
         ctx_lines = self._origin.order_line.mapped('id')
         ctx_lines1 = self.order_line.mapped('id')
         # if(len(ctx_lines) < len(ctx_lines1)):
