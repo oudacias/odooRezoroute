6f5a696985e33e35c323cbbd99de06d0eb5d24bc
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-09-01 15:27:12 +0100

    from mac

--- models/devis.py
+++ models/devis.py
@@ -114,7 +114,8 @@ class Devis(models.Model):
 
         else:
             # dict_check_lines[]
-            print("@@@ $$$$$$$$$ CHECKING multiple  lines from111  " + str(set(dict_check_lines.keys()).difference(dict_old_lines)))
+            # print("@@@ $$$$$$$$$ CHECKING multiple  lines from111  " + str(set(dict_check_lines.keys()).difference(dict_old_lines)))
+            print("@@@ $$$$$$$$$ CHECKING multiple  lines from111  " + str(set(dict_old_lines.keys()).difference(dict_check_lines)))
             # print("@@@ $$$$$$$$$ CHECKING multiple  lines from  " + str(dict_old_lines))
 
 
