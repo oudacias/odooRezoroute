9671450f7ee1aec782b5e315aff63df42aaaf25a
oudacias net <oudaciasnet@oudaciass-MacBook-Pro.local>
2022-09-02 13:32:45 +0100

    from mac

--- models/devis.py
+++ models/devis.py
@@ -133,6 +133,7 @@ class Devis(models.Model):
                 raise ValidationError('Vous ne pouvez pas supprimer cette ligne du forfait')
 
         else:
+            print("New @@@ ######## Check Order Line #####" + str(list(set(dict_check_lines.keys()).difference(self.dict_old_lines))))
             if(dict_check_lines[list(set(dict_check_lines.keys()).difference(self.dict_old_lines))[-1]] == False):
                 raise ValidationError('Vous ne pouvez pas supprimer cette ligne du forfait')
            
